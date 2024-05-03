cc905eedec006d6dfa910b752ab3e3925d5bb7bf
Suryansh <75418521+0Suryansh@users.noreply.github.com>
2024-05-03 18:16:21 +0530

    Update MoviewBox.jsx

--- src/components/MovieBox/MoviewBox.jsx
+++ src/components/MovieBox/MoviewBox.jsx
@@ -4,7 +4,7 @@ const MovieBox = () => {
     return(
         <div style={{width: "100%", height: "100%", backgroundColor: "black", textAlign:"center"}}>
             <img src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-symbol.jpg" alt="" style={{width: 150, height: 60, paddingTop: 10}}/>
-            <img src="https://highdefdiscnews.com/wp-content/uploads/2019/08/avengers_endgame_4k_41-1024x576.png" alt="" width={1000} height={600} style={{paddingTop: 10}}/>
+            <img src="https://highdefdiscnews.com/wp-content/uploads/2019/08/avengers_endgame_4k_41-1024x576.png" alt="" width={875} height={600} style={{paddingTop: 10}}/>
             
         </div>
     )
